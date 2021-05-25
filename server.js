c6723986d6b3c36eeb1004f18322e4c00fef185e
Yong Hong <yongghongg@hotmail.com>
2021-05-25 12:57:41 +0900

    set up user model and user registration

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// bodyParser middleware
+app.use(express.urlencoded());
+app.use(express.json());
 
 // DB config
 const db = require('./config/key').mongoURI;
